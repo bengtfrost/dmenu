ac42624571711e40cd79a8ed1d10620ac0f6afb6
fhblf <fhblf@tutanota.com>
2023-12-20 01:38:31 +0100

    changed colorscheme

--- config.def.h
+++ config.def.h
@@ -9,10 +9,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-  // [SchemeNorm] = { "#D2D2D2", "#383c4a" },
-  // [SchemeSel] = { "#ffffff", "#3b84de" },
+	// [SchemeNorm] = { "#bbbbbb", "#222222" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+  [SchemeNorm] = { "#D2D2D2", "#383c4a" },
+  [SchemeSel] = { "#ffffff", "#3b84de" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
