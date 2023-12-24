93431b43a7cc84788510f6382f77b131e9da3c0d
fhblf <fhblf@tutanota.com>
2023-12-24 16:08:29 +0100

    changed colorscheme

--- config.def.h
+++ config.def.h
@@ -10,7 +10,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+  [SchemeSel] = { "#eeeeee", "#0e5f88" },
   // [SchemeNorm] = { "#D2D2D2", "#383c4a" },
   // [SchemeSel] = { "#ffffff", "#3b84de" },
 	[SchemeOut] = { "#000000", "#00ffff" },
