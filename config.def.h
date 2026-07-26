332a1f9eadac56679b3c7b52befadc44d86d36a0
bengtfrost <bengtfrost@gmail.com>
2026-07-26 20:29:38 +0200

    min_lineheight fix

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static const char *colors[SchemeLast][2] = {
 static unsigned int lines = 0;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 8;
+static unsigned int min_lineheight = 6;
 
 /*
  * Characters not considered part of a word while deleting words
